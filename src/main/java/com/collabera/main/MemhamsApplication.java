8ce354b2a0457ca52c4a0e2031f49854384ca5d0
Devam M <devam493@gmail.com>
2019-06-28 13:05:05 -0400

    initial commit

---
+++ src/main/java/com/collabera/main/MemhamsApplication.java
@@ -0,0 +1,13 @@
+package com.collabera.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemhamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemhamsApplication.class, args);
+	}
+
+}
